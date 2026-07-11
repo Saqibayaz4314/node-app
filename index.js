429b40add0d27b2e65d8c6bfe63ec90265e4d0ce
Saqibayaz4314 <ayazs4314@gmail.com>
2026-07-11 15:14:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8080;
+
+app.get('/', (req, res)=>{
+  return res.json({msg: "Hello from the server"})
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${PORT}`)
+})
